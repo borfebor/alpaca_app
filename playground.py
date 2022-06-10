1f896c0eb47d6cc63c4a05ac5bdd1dd85efc4352
Borja <60699020+borfebor@users.noreply.github.com>
2022-06-10 19:14:29 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -41,19 +41,5 @@ if uploaded_file is not None:
     df, columns, default = alpaca.formater(df)
     col = st.sidebar.multiselect('Choose the columns to import', columns, default)
     df = df[col]
-
-placeholder = st.empty()
-
-# Replace the placeholder with some text:
-placeholder.text("Hello")
-
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
-
-# Replace the chart with several elements:
-with placeholder.container():
-     st.write("This is one element")
-     st.write("This is another")
-
-# Clear all those elements:
-#placeholder.empty()
+    cleaning = st.sidebar.expander('Data Cleaning & Manipulation', expanded=True)
+    
