824b1f668cd798dc3867ffd159d0191079686d32
Borja <60699020+borfebor@users.noreply.github.com>
2022-10-09 10:26:12 +0200

    Update playground.py

--- playground.py
+++ playground.py
@@ -54,7 +54,7 @@ if uploaded_file is not None:
     experimenter = st.expander('Experimental set-up', expanded=True)
     with experimenter:
           visualisation = st.selectbox('Feel free to explore a bit your data', ['Identified proteins', 'Intensity distribution'], 0)
-          fig = plt.figure(figsize=(10, 10))
+          fig = plt.figure()
           sns.catplot(y="Accession", x='Condition', hue='Replicate', data=df)
 
           st.pyplot(fig)
