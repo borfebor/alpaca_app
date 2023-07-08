5113debc4aa8b207edb2bdff993fa0159505ca33
Borja <60699020+borfebor@users.noreply.github.com>
2023-07-08 22:25:12 +0200

    Update viz.py

--- src/viz.py
+++ src/viz.py
@@ -99,9 +99,20 @@ class Viz:
         
         fitting = f'R2 = {round(R, 3)}'
 
-        chart = px.scatter(df, 
-                        x=df[amount], 
-                        y=df[lfq_method])
+        fig = go.Figure(data=go.Scatter(
+                df, 
+                x=df[amount], 
+                y=df[lfq_method])
+                mode='markers',
+                marker=dict(
+                    size=16,
+                    color=np.random.randn(500), #set color equal to a variable
+                    colorscale='Viridis', # one of plotly colorscales
+                    showscale=True
+                )
+            ))
+
+
         
         return chart
     
