c438fc85f6da012a20279f85de6a712ca22a5569
Borja <60699020+borfebor@users.noreply.github.com>
2023-07-09 11:45:10 +0200

    Update viz.py

--- src/viz.py
+++ src/viz.py
@@ -142,8 +142,8 @@ class Viz:
 
             traces = df[df['Condition'] == condition]
             chart.add_shape(
-                    dict(type="circle", x0=traces['x'].min(), y0=traces['y'].min(), 
-                         x1=traces['x'].max(), y1=traces['y'].max()), row=1, col="all", line_color="green"
+                    dict(type="circle", x0=traces[x].min(), y0=traces[y].min(), 
+                         x1=traces[x].max(), y1=traces[y].max()), row=1, col="all", line_color="green"
                 )
 
         chart.update_layout(
