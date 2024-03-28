d9a14cba1299d909169c0bea30df37cc3d938de9
Borja <borfebor@gmail.com>
2024-03-28 22:07:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,13 @@ if uploaded_file == None:
     
     example_data = st.sidebar.selectbox('Example datasets', [None, 'Ferrero-Bordera et al. 2024', 'Antelo-Varela et al. 2019'])
     st.sidebar.markdown("""
-                ## About us
+                ### [Tutorial and documentation](https://github.com/borfebor/alpaca_app)
+                
                 An app developed by [Microbial Proteomics lab](https://microbialproteomics.uni-greifswald.de/en/)
                 
-                ### [Tutorial and documentation](https://github.com/borfebor/alpaca_app)
                 """)
+                
+                
 
 if example_data != None:   
     
